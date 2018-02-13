d86d225caf621bf6731c053bddc8213b702b0878
EderV <EderV@github.com>
2018-02-13 22:52:30 +0100

    commit

--- app/src/main/java/com/app/a2/studio/android/stuff/your/order/orderyourstuff/SplashScreen.java
+++ app/src/main/java/com/app/a2/studio/android/stuff/your/order/orderyourstuff/SplashScreen.java
@@ -4,8 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
-    
-    private int numeroPrueba;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
