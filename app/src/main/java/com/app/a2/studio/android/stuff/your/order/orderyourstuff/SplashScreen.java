fbeecab5b6ec5bf09a480654e4c8f69e5604c1dc
EderV <EderV@github.com>
2018-02-13 20:39:36 +0100

    Initial commit

---
+++ app/src/main/java/com/app/a2/studio/android/stuff/your/order/orderyourstuff/SplashScreen.java
@@ -0,0 +1,13 @@
+package com.app.a2.studio.android.stuff.your.order.orderyourstuff;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
