8178f782d0276937b7b33a6e31597bc4828f6c98
Kirtasth <36455523+Kirtasth@users.noreply.github.com>
2018-02-14 13:58:52 +0100

    Comentario david

--- app/src/main/java/com/app/a2/studio/android/stuff/your/order/orderyourstuff/BaseDeDatos/BaseDeDatos.java
+++ app/src/main/java/com/app/a2/studio/android/stuff/your/order/orderyourstuff/BaseDeDatos/BaseDeDatos.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-
+//  Comentario david
 public class BaseDeDatos extends SQLiteOpenHelper {
 
 
