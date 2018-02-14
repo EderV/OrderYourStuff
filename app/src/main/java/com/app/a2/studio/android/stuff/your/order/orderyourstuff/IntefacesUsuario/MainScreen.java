42b2be078a8195a19236db42e59898ffd3a53ed7
EderV <EderV@github.com>
2018-02-14 12:37:01 +0100

    commit2

--- app/src/main/java/com/app/a2/studio/android/stuff/your/order/orderyourstuff/IntefacesUsuario/MainScreen.java
+++ app/src/main/java/com/app/a2/studio/android/stuff/your/order/orderyourstuff/IntefacesUsuario/MainScreen.java
@@ -2,8 +2,6 @@ package com.app.a2.studio.android.stuff.your.order.orderyourstuff.IntefacesUsuar
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.app.a2.studio.android.stuff.your.order.orderyourstuff.BaseDeDatos.BaseDeDatos;
 import com.app.a2.studio.android.stuff.your.order.orderyourstuff.R;
 
 public class MainScreen extends AppCompatActivity {
@@ -12,7 +10,5 @@ public class MainScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
-
-
     }
 }
