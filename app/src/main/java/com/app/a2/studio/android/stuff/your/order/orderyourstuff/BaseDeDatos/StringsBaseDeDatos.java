8362648f305457b532ae9dc7b09e941a3cc86b5b
EderV <EderV@github.com>
2018-02-14 13:49:20 +0100

    commit

--- app/src/main/java/com/app/a2/studio/android/stuff/your/order/orderyourstuff/BaseDeDatos/StringsBaseDeDatos.java
+++ app/src/main/java/com/app/a2/studio/android/stuff/your/order/orderyourstuff/BaseDeDatos/StringsBaseDeDatos.java
@@ -1,7 +1,7 @@
 package com.app.a2.studio.android.stuff.your.order.orderyourstuff.BaseDeDatos;
 
 
-public class StringsBaseDeDatos {
+public abstract class StringsBaseDeDatos {
 
     public static final String DATABASE_NAME = "OrderYurStuffDataBase.db";
     public static final int DATABASE_VERSION = 1;
