0c5c2e0353f96e292319f659bb2dfba16b56ed94
EderV <EderV@github.com>
2018-02-18 16:06:58 +0100

    f

--- app/src/main/java/com/app/a2/studio/android/stuff/your/order/orderyourstuff/IntefacesUsuario/SplashScreen.java
+++ app/src/main/java/com/app/a2/studio/android/stuff/your/order/orderyourstuff/IntefacesUsuario/SplashScreen.java
@@ -6,23 +6,12 @@ import android.os.Bundle;
 import com.app.a2.studio.android.stuff.your.order.orderyourstuff.R;
 
 public class SplashScreen extends AppCompatActivity {
-    //  Atributtes
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-    }
-
-
-    private void metodo(){
-        int a = 0, b = 0;
-        boolean es;
-        a=a++;
-        b=++b;
-        es=(a==b);
     }
 
 }
