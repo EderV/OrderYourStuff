dfe14fc4ee9ff6d80b0d29e9b4807b3aff07e773
EderV <EderV@github.com>
2018-02-20 13:57:09 +0100

    Trabajando con las interfaces de usuario

--- app/src/main/java/com/app/a2/studio/android/stuff/your/order/orderyourstuff/IntefacesUsuario/SplashScreen.java
+++ app/src/main/java/com/app/a2/studio/android/stuff/your/order/orderyourstuff/IntefacesUsuario/SplashScreen.java
@@ -1,5 +1,7 @@
 package com.app.a2.studio.android.stuff.your.order.orderyourstuff.IntefacesUsuario;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,6 +13,15 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainScreen.class);
+                startActivity(i);
+                SplashScreen.this.finish();
+            }
+        }, 2000);
 
     }
 
